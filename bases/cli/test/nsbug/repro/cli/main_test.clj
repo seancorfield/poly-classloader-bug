5e9bd9fcf58525d8de3124e65f95f5fb098c07e3
Sean Corfield <sean@corfield.org>
2022-08-11 08:19:08 -0700

    add repro function

--- bases/cli/test/nsbug/repro/cli/main_test.clj
+++ bases/cli/test/nsbug/repro/cli/main_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer [deftest testing is]]
             [nsbug.repro.cli.main :as sut]))
 
+(defn foo []
+  (clojure.core.server/repl))
+
 (deftest a-test
   (testing "FIXME, I do nothing."
     (is (= 0 0))))
